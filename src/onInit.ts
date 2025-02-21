ff9fdf16d4756a0c2d37bdb23b2bc5ac0430ca05
ZuperZee <ole.kristian.kaaseth@gmail.com>
2025-02-21 16:30:46 +0100

    Add chart js code

--- src/onInit.ts
+++ src/onInit.ts
@@ -1,1 +1,29 @@
+import { Chart } from "chart.js/auto";
 import "./style.css";
+
+const chartElt = htmlNode.querySelector<HTMLCanvasElement>("#chart-canvas");
+
+if (!chartElt) {
+  throw new Error("No chart element found");
+}
+
+new Chart(chartElt, {
+  type: "bar",
+  data: {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
